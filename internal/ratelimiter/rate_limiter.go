e57b26adb3843a256cf4e67cb5cbaa066a625e86
Hanzala Javed <hanzalaoc211@gmail.com>
2025-11-18 00:30:54 +0500

    removed extra code

--- internal/ratelimiter/rate_limiter.go
+++ internal/ratelimiter/rate_limiter.go
@@ -2,10 +2,6 @@ package ratelimiter
 
 import "time"
 
-type Limiter interface {
-	Allow(ip string) (bool, time.Duration)
-}
-
 type RateLimiterConfig struct {
 	MaxRequests int
 	Duration    time.Duration
